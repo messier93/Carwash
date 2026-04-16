08ac4cb9e4d5641c9a2d3136d23a1c79e35c2d7f
messier93 <dyasarum29@gmail.com>
2026-04-17 01:13:25 +0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- antriancucimobil.py
+++ antriancucimobil.py
@@ -1,41 +1,83 @@
-#Car Wash
+#file
 
-queueList = []
-completedList = []
-maxQueue = 2
+namaFile = "carwash.txt" 
 
-prices = {}
-#Customer
-def customerMenu():
-    print("=== CUSTOMER MENU ===")
-    print("1. Ambil Antrian")
-    print("2. Cek Urutan ")
-    print("0. Kembali")
+def bacaData(namaFile):
+    dataList = []
 
-choice = input("Pilih menu: ")
-if choice == "1":
-    takeQueue()
-elif choice == "2":
-    searchList()
-elif choice == "3":
-    return
-else:
-    print("Menu tidak valid.")
+    try:
+        with open(namaFile, "r") as file:
+            for baris in file:
+                baris = baris.strip()
 
-def takeQueue():
-    global queueList
+                if baris == "":
+                    continue
 
-    if len(queueList) >= maxQueue
-    print("Maaf, kuota hari ini sudah penuh. Silahkan kembali besok.")
+                nama, status = baris.split(",")
+
+                dataList.append({
+                    "nama": nama,
+                    "status": status
+                })
+
+    except FileNotFoundError:
+        pass
+        
+    return dataList
+
+#save antrian baru
+def simpanData(namaFile, dataList):
+    with open(namaFile, "w") as file:
+        for data in dataList:
+            file.write(f"{data['nama']},{data['status']}\n")
+
+#baca antrian
+def tampilkanAntrian(dataList):
+    if len(dataList) == 0:
+        print("Antrian kosong.")
+        return
+
+    print("DAFTAR ANTRIAN")
+    for i, data in enumerate(dataList, start=1):
+        print(f"{i}. {data['nama']} - {data['status']}")
+
+#add antrian
+def tambahAntrian(dataList):
+    nama = input("Masukkan nama: ").strip()
+
+    if nama == "":
+        print("Nama tidak boleh kosong.")
+        return
+
+    dataList.append({
+        "nama": nama,
+        "status": "Menunggu"
+    })
+
+    print("Berhasil ditambahkan.")
+
+#update status antrian
+def updateStatus(dataList, history):
+    nama = input("Masukkan nama: ").strip()
+
+    for data in dataList:
+        if data["nama"] == nama:
+
+            print("1. Sedang Dicuci")
+            print("2. Selesai")
+
+            pilihan = input("Pilih status: ").strip()
 
 
+#delete antrian
+def hapusAntrian(dataList):
+    nama = input("Masukkan nama: ").strip()
 
+    for data in dataList:
+        if data["nama"] == nama:
+            dataList.remove(data)
+            print("Data berhasil dihapus.")
+            return
 
-#Admin
-def adminMenu():
-    print("=== ADMIN MENU ===")
-    print("1. Ambil Antrian")
-    print("2. Cek Urutan ")
-    print("0. Kembali")
+    print("Data tidak ditemukan.")
 
-choice = input("Pilih menu: ")
