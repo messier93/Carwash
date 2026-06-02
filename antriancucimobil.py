8368f31978700eeafeb90dd2a348e84a6d558759
messier93 <dyasarum29@gmail.com>
2026-06-03 01:17:55 +0700

    Add date tracking to car wash queue management

--- antriancucimobil.py
+++ antriancucimobil.py
@@ -4,8 +4,10 @@
 #         Haura Nur Hafizhah
 
 #file
+from datetime import datetime
 
 namaFile = "carwash.txt" 
+historyFile = "history.txt"
 
 def bacaData(namaFile):
     dataList = []
@@ -18,12 +20,13 @@ def bacaData(namaFile):
                 if baris == "":
                     continue
 
-                plat, status, pembayaran = baris.split(",")
+                plat, status, pembayaran, tanggal = baris.split(",")
 
                 dataList.append({
                     "plat": plat,
                     "status": status,
-                    "pembayaran": pembayaran
+                    "pembayaran": pembayaran,
+                    "tanggal": tanggal
                 })
 
     except FileNotFoundError:
@@ -31,12 +34,37 @@ def bacaData(namaFile):
         
     return dataList
 
+#historyy
+def bacaHistory():
+    history = []
+    try:
+        with open(historyFile, "r") as file:
+            for baris in file:
+                plat, status, pembayaran, tanggal = baris.strip().split(",")
+
+                history.append({
+                    "plat": plat,
+                    "status": status,
+                    "pembayaran": pembayaran,
+                    "tanggal": tanggal
+                })
+                
+    except FileNotFoundError:
+        pass
+
+    return history
+    
 #save antrian baru
 def simpanData(namaFile, dataList):
     with open(namaFile, "w") as file:
         for data in dataList:
-            file.write(f"{data['plat']},{data['status']},{data['pembayaran']}\n")
-
+            file.write(f"{data['plat']},{data['status']},{data['pembayaran']},{data['tanggal']}\n")
+            
+def simpanHistory(history):
+    with open(historyFile, "w") as file:
+        for d in history:
+            file.write(f"{d['plat']},{d['status']},{d['pembayaran']},{d['tanggal']}\n")
+            
 #add antrian
 def tambahAntrian(dataList):
     plat = input("Masukkan nomor plat kendaraan: ").strip()
@@ -62,10 +90,12 @@ def tambahAntrian(dataList):
     dataList.append({
         "plat": plat,
         "status": "Menunggu",
-        "pembayaran": pembayaran
+        "pembayaran": pembayaran,
+        "tanggal": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     })
-
-    print("\n✓ Antrian berhasil ditambahkan.")
+    
+    simpanData(namaFile, dataList)
+    print("\nAntrian berhasil ditambahkan.")
 
 #baca/read antrian
 def tampilkanAntrian(dataList):
@@ -76,7 +106,7 @@ def tampilkanAntrian(dataList):
     print("\n" + "="*60)
     print("                 DAFTAR ANTRIAN")
     print("="*60)
-    print(f"{'No':<5}{'Plat':<15}{'Status':<20}{'Pembayaran'}")
+    print(f"{'No':<5}{'Plat':<15}{'Status':<20}{'Pembayaran':<15}{'Tanggal'}")
     print("-"*60)
 
     for i, data in enumerate(dataList, start=1):
@@ -84,12 +114,12 @@ def tampilkanAntrian(dataList):
             f"{i:<5}"
             f"{data['plat']:<15}"
             f"{data['status']:<20}"
-            f"{data['pembayaran']}"
+            f"{data['pembayaran']:<15}"
+            f"{data['tanggal']}"
         )
 
     print("="*60)
-
-
+    
 #update status antrian
 def prosesAntrian(dataList, history):
     if len(dataList) == 0:
@@ -116,11 +146,14 @@ def prosesAntrian(dataList, history):
         history.append(data)   # stack (riwayat)
         dataList.pop(0)        # queue (fifo)
 
-        print("\n✓ Mobil selesai dicuci dan masuk ke riwayat.")
+        print("\nMobil selesai dicuci dan masuk ke riwayat.")
 
     else:
         print("Pilihan tidak valid.")
-
+        
+    simpanHistory(history)
+    simpanData(namaFile, dataList)
+    
 #delete antrian
 def hapusAntrian(dataList):
     plat = input("Masukkan plat: ").strip().upper()
@@ -128,25 +161,43 @@ def hapusAntrian(dataList):
     for data in dataList:
         if data["plat"].upper() == plat:
             dataList.remove(data)
-            print("\n✓ Data kendaraan berhasil dihapus.")
+            simpanData(namaFile, dataList)
+            print("\nData kendaraan berhasil dihapus.")
             return
 
-    print("\n✗ Plat tidak ditemukan.")
+    print("\nPlat tidak ditemukan.")
 
 #show history dgn stack
-def tampilkanHistory(history):
+def tampilkanHistoryAkhir(history):
     if len(history) == 0:
         print("Belum ada riwayat.")
         return
 
     print("\n" + "="*35)
-    print("      RIWAYAT CAR WASH")
+    print("      RIWAYAT CAR WASH HDC (Terakhir Selesai)")
     print("="*35)
 
-    # LIFO
     for i, data in enumerate(reversed(history), start=1):
-        print(f"{i}. {data['plat']}")
+        print(f"{i}. {data['plat']} | {data['tanggal']}")
 
+#report berdasarkan tanggal
+def tampilkanHistoryDate(history):
+    if len(history) == 0:
+        print("Belum ada riwayat.")
+        return
+
+    print("\n" + "="*35)
+    print("      RIWAYAT CAR WASH HDC (Berdasarkan Tanggal)")
+    print("="*35)
+
+    historyUrut = sorted(
+        history,
+        key=lambda x: datetime.strptime(x["tanggal"].strip(), "%d-%m-%Y %H:%M:%S")
+    )
+
+    for i, data in enumerate(historyUrut, start=1):
+        print(f"{i}. {data['plat']} | {data['tanggal']}")
+            
 #search
 def cariPlat(dataList):
     platCari = input("Masukkan plat yang dicari: ").strip().upper()
@@ -159,6 +210,7 @@ def cariPlat(dataList):
             print(f"Plat        : {data['plat']}")
             print(f"Status      : {data['status']}")
             print(f"Pembayaran  : {data['pembayaran']}")
+            print(f"Tanggal     : {data['tanggal']}")
             print("="*35)
             return
 
@@ -167,7 +219,7 @@ def cariPlat(dataList):
 #main program
 def main():
     dataList = bacaData(namaFile)
-    history = []
+    history = bacaHistory()
     
     while True:
         print("\n" + "="*40)
@@ -178,15 +230,14 @@ def main():
         print("3. Proses Antrian")
         print("4. Hapus Antrian")
         print("5. Cari Plat")
-        print("6. Lihat Riwayat")
-        print("7. Simpan ke File")
+        print("6. Lihat Riwayat Terakhir")
+        print("7. Lihat Riwayat Berdasarkan Tanggal")
         print("0. Keluar")
         print("="*40)
 
         pilihan = input("Pilih menu: ").strip()
-
         if pilihan == "1":
-            tambahAntrian(dataList)
+            tambahAntrian(dataLikalst)
         elif pilihan == "2":
             tampilkanAntrian(dataList)
         elif pilihan == "3":
@@ -196,12 +247,12 @@ def main():
         elif pilihan == "5":
             cariPlat(dataList)
         elif pilihan == "6":
-            tampilkanHistory(history)
+            tampilkanHistoryAkhir(history)
         elif pilihan == "7":
-            simpanData(namaFile, dataList)
-            print("Data disimpan.")
+            tampilkanHistoryDate(history)
         elif pilihan == "0":
             simpanData(namaFile, dataList)
+            simpanHistory(history)
             print("Program selesai.")
             break
         else:
